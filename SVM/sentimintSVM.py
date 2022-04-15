c330682e931d507ffeb36e756759ebf61a248c8a
JadenNosk <jadhawgame@gmail.com>
2022-04-14 21:32:45 -0500

    atarted SVM

---
+++ SVM/sentimintSVM.py
@@ -0,0 +1,11 @@
+import sklearn
+from sklearn import svm
+class sentimintSVM:
+    def SVM_train(self, data_file_name):
+        pass
+
+    def SVM_test(self, data_file_name):
+        pass
+
+    def __init__(self, data_file_name):
+        pass
