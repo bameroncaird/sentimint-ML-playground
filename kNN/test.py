0f20dce1c272011329f6cc1a161e6fb245a6d7b4
JadenNosk <jadhawgame@gmail.com>
2022-05-15 09:11:39 -0500

    updated test file for kNN

--- kNN/test.py
+++ kNN/test.py
@@ -1,1 +1,9 @@
+# Import SentiMint_kNN, do NOT import any other kNN files
+import SentiMint_kNN
 
+# To use the algorithm you must first create an instance of sentiMint_kNN
+kNN = SentiMint_kNN.sentiMint_kNN()
+
+# use .predict() to get a score {-100, 0, 100} on a string
+# pass a SINGLE STRING. it will return a number
+print(str(kNN.predict("Good day")))
